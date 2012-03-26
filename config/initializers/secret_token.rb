eb1e99e544647330a5419ecec683e51a5440696c
Reed Law <reed@smashingboxes.com>
2012-03-26 13:44:42 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = 'cf6dbb0354ec8d2b257115e8707f5d1a2a66e711bbb3c66a876a00a7935bbd9150c68e704be6729227bf92e20a60832aa7527ec2987290236ec30a93993d8a51'
